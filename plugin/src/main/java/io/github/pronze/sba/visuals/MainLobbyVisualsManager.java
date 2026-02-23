4a3c64378325beec8ade35d3314718ec2aa554a1
denis5135 <109866888+denis5135@users.noreply.github.com>
2026-02-24 00:23:31 +0500

    Update MainLobbyVisualsManager.java

--- plugin/src/main/java/io/github/pronze/sba/visuals/MainLobbyVisualsManager.java
+++ plugin/src/main/java/io/github/pronze/sba/visuals/MainLobbyVisualsManager.java
@@ -31,6 +31,8 @@ import io.github.pronze.sba.config.SBAConfig;
 import io.github.pronze.sba.utils.SBAUtil;
 import io.github.pronze.sba.utils.ShopUtil;
 import io.github.pronze.lib.pronzelib.scoreboards.Scoreboard;
+import io.github.pronze.sba.levels.PlayerLevelManager;
+
 import java.util.*;
 
 @Service
@@ -121,9 +123,7 @@ public class MainLobbyVisualsManager implements Listener {
                         p.sendMessage(msgToSend);
                 });
 
-                // e.getRecipients().clear();
                 e.setCancelled(true);
-                // e.setFormat(format);
             }
         }
     }
@@ -133,7 +133,6 @@ public class MainLobbyVisualsManager implements Listener {
         Set.copyOf(scoreboardMap.keySet()).forEach(this::remove);
         scoreboardMap.clear();
         enabled = false;
-        // HandlerList.unregisterAll(this);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -198,44 +197,107 @@ public class MainLobbyVisualsManager implements Listener {
             playerData.sendPlayerListHeaderFooter(header, footer);
         }
 
-        var title = LanguageService
-                .getInstance()
-                .get(MessageKeys.MAIN_LOBBY_SCOREBOARD_TITLE)
-                .toString();
-
-        var lines = LanguageService
-                .getInstance()
-                .get(MessageKeys.MAIN_LOBBY_SCOREBOARD_LINES)
-                .toStringList();
+        // Анимированный заголовок "Bedwars"
+        List<String> animatedTitle = new ArrayList<>();
+        animatedTitle.add("&6&lB&e&led&6&lw&e&lar&6&ls");
+        animatedTitle.add("&e&lB&6&led&e&lw&6&lar&e&ls");
+        animatedTitle.add("&6&lBe&e&ldw&6&lar&e&ls");
+        animatedTitle.add("&e&lBed&6&lw&e&lar&e&ls");
+        animatedTitle.add("&6&lBedw&e&lar&6&ls");
+        animatedTitle.add("&e&lBedwa&6&lr&e&ls");
+        animatedTitle.add("&6&lBedwar&e&ls");
+        animatedTitle.add("&e&lBedwars");
 
         final var scoreboard = Scoreboard.builder()
-                .animate(false)
+                .animate(true)
                 .player(player)
-                .title(title)
+                .title(animatedTitle)
                 .displayObjective(MAIN_LOBBY_OBJECTIVE)
                 .updateInterval(20L)
-                .lines(lines)
+                .lines(getScoreboardLines())
                 .placeholderHook(hook -> {
-                    final var bar = playerData.getCompletedBoxes();
-                    final var progress = playerData.getProgress();
+                    // Используем нашу новую систему уровней
+                    var levelManager = PlayerLevelManager.getInstance();
+                    int playerLevel = levelManager.getPlayerLevel(player);
+                    String playerPrefix = levelManager.getPlayerPrefix(player);
+                    int playerXP = levelManager.getPlayerXP(player);
+                    int xpToNext = levelManager.getXPToNextLevel(player);
+                    double progress = levelManager.getLevelProgress(player);
+                    
+                    // Создаём прогресс-бар (10 символов)
+                    int barLength = 10;
+                    int filledBars = (int) Math.round(progress * barLength);
+                    StringBuilder bar = new StringBuilder("§a");
+                    for (int i = 0; i < barLength; i++) {
+                        if (i < filledBars) {
+                            bar.append("■");
+                        } else {
+                            bar.append("§7■");
+                        }
+                    }
+                    
                     final var playerStatistic = Main
                             .getPlayerStatisticsManager()
                             .getStatistic(player);
+                            
+                    int totalGames = playerStatistic.getWins() + playerStatistic.getLosses();
+                    double winRate = totalGames > 0 ? (double) playerStatistic.getWins() / totalGames * 100 : 0;
 
                     return hook
                             .getLine()
                             .replace("%sba_version%", SBA.getInstance().getVersion())
                             .replace("%kills%", String.valueOf(playerStatistic.getKills()))
-                            .replace("%beddestroys%", String.valueOf(playerStatistic.getDestroyedBeds()))
+                            .replace("%beds%", String.valueOf(playerStatistic.getDestroyedBeds()))
                             .replace("%deaths%", String.valueOf(playerStatistic.getDeaths()))
-                            .replace("%level%", "§7" + playerData.getLevel() + "✫")
-                            .replace("%progress%", progress)
-                            .replace("%bar%", bar)
+                            // Уровни
+                            .replace("%level%", playerPrefix + " " + playerLevel + "✫")
+                            .replace("%xp%", String.valueOf(playerXP))
+                            .replace("%xp_required%", String.valueOf(xpToNext))
+                            .replace("%progress%", String.valueOf((int)(progress * 100)) + "%")
+                            .replace("%bar%", bar.toString())
+                            // Победы/поражения
                             .replace("%wins%", String.valueOf(playerStatistic.getWins()))
-                            .replace("%kdr%", String.valueOf(playerStatistic.getKD()));
+                            .replace("%losses%", String.valueOf(playerStatistic.getLosses()))
+                            .replace("%games%", String.valueOf(totalGames))
+                            .replace("%winrate%", String.format("%.1f", winRate) + "%")
+                            // K/D
+                            .replace("%kdr%", String.valueOf(playerStatistic.getKD()))
+                            // Привилегия (заглушка, можно заменить на реальную систему доната)
+                            .replace("%rank%", "&6[VIP]")
+                            .replace("%donate%", "&6[VIP]");
                 }).build();
 
         scoreboardMap.put(player, scoreboard);
+    }
+
+    private List<String> getScoreboardLines() {
+        List<String> lines = new ArrayList<>();
+        
+        // Твоя привилегия
+        lines.add("&6%rank%");
+        // Пустая строка
+        lines.add("");
+        // Уровень
+        lines.add("&7Уровень: %level%");
+        // Опыт
+        lines.add("&7Опыт: %xp%/%xp_required%");
+        // Прогресс-бар
+        lines.add("%bar%");
+        // Пустая строка
+        lines.add("");
+        // Статистика
+        lines.add("&7Всего убийств: &a%kills%");
+        lines.add("&7Всего побед: &a%wins%");
+        lines.add("&7Всего кроватей: &a%beds%");
+        lines.add("&7У/С: &a%kdr%");
+        lines.add("&7В/П: &a%wins%&7/&c%losses% &8(%winrate%)");
+        lines.add("&7Всего игр: &a%games%");
+        // Пустая строка
+        lines.add("");
+        // Айпи
+        lines.add("&bplay.yourserver.com");
+        
+        return lines;
     }
 
     public void remove(Player player) {
@@ -265,11 +327,5 @@ public class MainLobbyVisualsManager implements Listener {
         final var player = e.getPlayer();
         if (!enabled)
             return;
-        // the Slib 2.0.2-SNAPSHOT version of the following code did not have the .start() call, therefore was never functional. Is it needed?
-//        Tasker.runDelayed(DefaultThreads.GLOBAL_THREAD, () -> {
-//            if (isInWorld(player.getLocation()) && player.isOnline()) {
-//                create(player);
-//            }
-//        }, 1L, TaskerTime.SECONDS);
     }
 }
