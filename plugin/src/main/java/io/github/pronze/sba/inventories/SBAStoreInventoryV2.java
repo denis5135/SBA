4e1b7c4a0d94769a97e3d977f7190c8e985cd0cd
denis5135 <109866888+denis5135@users.noreply.github.com>
2026-03-01 10:04:10 +0500

    Update SBAStoreInventoryV2.java

--- plugin/src/main/java/io/github/pronze/sba/inventories/SBAStoreInventoryV2.java
+++ plugin/src/main/java/io/github/pronze/sba/inventories/SBAStoreInventoryV2.java
@@ -611,8 +611,8 @@ public class SBAStoreInventoryV2 extends AbstractStoreInventory {
                     }
                     
                     if (toolType != null) {
-                        // Получаем цену из itemInfo
-                        int price = itemInfo.getPrice();
+                        // Получаем цену из amount материала
+                        int price = materialItem.get().getAmount();
                         boolean success = ToolUpgradeManager.getInstance().upgradeTool(player, toolType, type, price);
                         if (!success) {
                             shouldSellStack = false;
